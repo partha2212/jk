dabf6de3bc5c2bf4672b142aec96f03fc933e106
partha2212 <riskstrikeback@gmail.com>
2025-06-10 15:41:36 +0530

    Add files via upload

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    int a=10,b=20,res;
+    res=a+b;
+        System.out.println( "Hthe sum"+res );
+    }
+}
